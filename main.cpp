04e7ff08806cf40034e447e172023e33592c73ca
“Aw1613” <87960001+Aw1613@users.noreply.github.com>
2026-03-23 22:50:44 +0530

    Grid create

--- main.cpp
+++ main.cpp
@@ -17,3 +17,70 @@ Frame-work
 Map randomizer :- genrates dynamic maps 
 
 */
+
+
+#include<iostream>
+using namespace std;
+
+// Grid setup
+class Grid{
+    public:
+        char arr[10][10];   // matrix size defined 
+        // void value_setter(Grid obj){
+        //     obj.arr[3][5] = 'X';
+        //     obj.arr[7][1] = 'X';
+        // }
+        
+        // void gird_display(void){
+        // cout<<"     |  .  .  .  .  .  .  .  .  .  .  |\n"
+        // <<"     |  .  .  .  .  .  .  .  .  .  .  |\n"
+        // <<"     |  .  .  .  .  .  .  .  .  .  .  |\n"
+        // <<"     |  .  .  .  .  .  .  .  .  .  .  |\n"
+        // <<"     |  .  .  .  .  .  .  .  .  .  .  |\n"
+        // <<"     |  .  .  .  .  .  .  .  .  .  .  |\n"
+        // <<"     |  .  .  .  .  .  .  .  .  .  .  |\n"
+        // <<"     |  .  .  .  .  .  .  .  .  .  .  |\n"
+        // <<"     |  .  .  .  .  .  .  .  .  .  .  |\n"
+        // <<"     |  .  .  .  .  .  .  .  .  .  .  |\n";
+        // }
+        void grid_disp_logic(Grid obj){
+            obj.arr[3][5] = 'X';
+            obj.arr[7][1] = 'X';
+            for (int i = 0; i < 10; i++)
+            {
+                cout<<"| ";
+                for (int j = 0; j < 10; j++)
+                {
+                    if (obj.arr[i][j] == 'S')
+                    {
+                        obj.arr[i][j] = 'S';
+                        cout<<" S ";
+                    }
+                    else if (obj.arr[i][j] == 'E'){
+                        obj.arr[i][j] = 'E';
+                        cout<<" E ";
+                    }
+                    else if(obj.arr[i][j] == 'X'){
+                        obj.arr[i][j] = 'x';
+                        cout<<" X ";
+                    }
+                    else{
+                        obj.arr[i][j] = '.';
+                        cout<<" . ";
+                    }
+                    
+                }
+                cout<<"|"<<endl;
+                
+            }
+            
+        }
+};
+
+int main(){
+    Grid g1;
+    cout<<"This is the grid \n\n\n\n";
+    g1.grid_disp_logic(g1);
+    cout<<"\n\n\n\n";
+    return 0;
+}
