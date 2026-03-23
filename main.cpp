75cc7995913c6b8112a7c39fc4de46f182e51d05
“Aw1613” <87960001+Aw1613@users.noreply.github.com>
2026-03-23 12:18:21 +0530

    Framework added

---
+++ main.cpp
@@ -0,0 +1,19 @@
+// Warehouse path finder 
+
+/*
+Frame-work 
+
+1.  10 x 10 gird on CLI using array 
+2.  cell type declaratin in bools
+3.  neighbour accesing funciton (all 4 direction)
+
+4. BFS algorthm implementaion
+4(b).   {If BFS succesfull } A* implemtntaion 
+
+5. Robot (location + path + cell type change)
+6. Connecting algorithms to Robot
+
+/////////  For later
+Map randomizer :- genrates dynamic maps 
+
+*/
