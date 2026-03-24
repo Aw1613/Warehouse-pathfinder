e5b0c67f77cc9663af91085616a08c15819f82f0
“Aw1613” <87960001+Aw1613@users.noreply.github.com>
2026-03-24 16:47:16 +0530

    Grid Ready

--- main.cpp
+++ main.cpp
@@ -25,27 +25,56 @@ using namespace std;
 // Grid setup
 class Grid{
     public:
+        int obst;
         char arr[10][10];   // matrix size defined 
         // void value_setter(Grid obj){
         //     obj.arr[3][5] = 'X';
         //     obj.arr[7][1] = 'X';
         // }
         
-        // void gird_display(void){
-        // cout<<"     |  .  .  .  .  .  .  .  .  .  .  |\n"
-        // <<"     |  .  .  .  .  .  .  .  .  .  .  |\n"
-        // <<"     |  .  .  .  .  .  .  .  .  .  .  |\n"
-        // <<"     |  .  .  .  .  .  .  .  .  .  .  |\n"
-        // <<"     |  .  .  .  .  .  .  .  .  .  .  |\n"
-        // <<"     |  .  .  .  .  .  .  .  .  .  .  |\n"
-        // <<"     |  .  .  .  .  .  .  .  .  .  .  |\n"
-        // <<"     |  .  .  .  .  .  .  .  .  .  .  |\n"
-        // <<"     |  .  .  .  .  .  .  .  .  .  .  |\n"
-        // <<"     |  .  .  .  .  .  .  .  .  .  .  |\n";
-        // }
         void grid_disp_logic(Grid obj){
-            obj.arr[3][5] = 'X';
-            obj.arr[7][1] = 'X';
+            // For user input based obstacle course , uncomment it 
+
+
+            // // First taking input from the user to place the obstacles on the map 
+            // cout<<"Please enter the number of obstacles you want in the course :"<<endl;
+            // cin>>obst;
+            // int h = 0;
+            // int k = 0;
+            // cout<<"Please enter the cordinates for the obstacles in X and Y :"<<endl;
+            // // This will display the obstacle acording to the user input 
+            // for (int i = 0; i < obst; i++)
+            // {
+            //     cout<<"Row "<<i+1<<" : ";
+            //     cin>>h;
+            //     cout<<"Column "<<i+1<<" : ";
+            //     cin>>k;
+            //     cout<<endl;
+            //     obj.arr[h][k] = 'X';
+            // }
+            
+
+            // Hardcoded map for testing 
+
+            obj.arr[0][0] = 'S';
+
+            obj.arr[1][3] = 'X';
+            obj.arr[2][3] = 'X';
+            obj.arr[3][3] = 'X';
+            obj.arr[4][3] = 'X';
+            obj.arr[4][4] = 'X';
+            obj.arr[4][5] = 'X';
+
+            obj.arr[2][7] = 'X';
+            obj.arr[3][7] = 'X';
+            obj.arr[3][6] = 'X';
+
+            obj.arr[7][8] = 'X';
+            obj.arr[8][8] = 'X';
+
+            obj.arr[9][9] = 'E';
+
+
             for (int i = 0; i < 10; i++)
             {
                 cout<<"| ";
